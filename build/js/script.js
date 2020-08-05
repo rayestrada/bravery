bd92ef5559cc648bbcbe6e3974cc480b91692d8d
Candice Dexter <candice.dexter@u.group>
2020-08-05 12:26:08 -0400

    Compiling build files.

---
+++ build/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Theme Javascript file for initializing scripts.
+ */
+// Standard wrapper
+// (() => {
+//   // Place code here.
+// }());
+// jQuery wrapper example.
+// ($ => {
+//   // DOC READY.
+//   $(() => {
+//   // Place code here.
+// });
+// })(jQuery);
+"use strict";
